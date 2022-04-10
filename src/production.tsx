96789fc28f7ff07e038b4227d2957b2a98574914
Grigory <gob@live.ru>
2022-04-11 01:51:03 +0500

    v0.1.1

---
+++ src/production.tsx
@@ -0,0 +1,39 @@
+import DuiAccordion from './components/DuiAccordion';
+import DuiBadge from './components/DuiBadge';
+import DuiButton from './components/DuiButton';
+import DuiCard from './components/DuiCard';
+import DuiCheckBox from './components/DuiCheckBox';
+import DuiCode from './components/DuiCode';
+import DuiControlsGroup from './components/DuiControlsGroup';
+import DuiInput from './components/DuiInput';
+import DuiMenu from './components/DuiMenu';
+import DuiProgress from './components/DuiProgress';
+import DuiRadioGroup from './components/DuiRadioGroup';
+import DuiSelect from './components/DuiSelect';
+import DuiTextarea from './components/DuiTextarea';
+
+import DuiConfirm from './components/modals/DuiConfirm';
+import DuiModal from './components/modals/DuiModal';
+
+import DuiSquareLoader from './components/loaders/DuiSquareLoader';
+import DuiSvgSpinner from './components/loaders/DuiSvgSpinner';
+
+export {
+  DuiAccordion,
+  DuiBadge,
+  DuiButton,
+  DuiCard,
+  DuiCheckBox,
+  DuiCode,
+  DuiControlsGroup,
+  DuiInput,
+  DuiMenu,
+  DuiProgress,
+  DuiRadioGroup,
+  DuiSelect,
+  DuiTextarea,
+  DuiConfirm,
+  DuiModal,
+  DuiSquareLoader,
+  DuiSvgSpinner,
+};
